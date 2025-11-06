093fd09aaa6ba46c64596c050ac9ba6a4e8997d2
Gürkan <gurkandizen17@gmail.com>
2025-11-06 15:12:13 +0300

    süre animasyonu & start button

--- js/app.js
+++ js/app.js
@@ -8,26 +8,41 @@ const soruListesi = [
 const quiz = new Quiz(soruListesi);
 const ui = new UI();
 
+ui.btnStart.addEventListener("click", function() {
+    startTimer(10);
+    startTimerLine();
+
+    ui.quizBox.classList.add("active");
+    ui.buttonBox.classList.remove("active");
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.btnNext.classList.remove("show");
+});
+
 ui.btnNext.addEventListener("click", function() {
     if(quiz.sorular.length != quiz.soruIndex) {
+        startTimer(10);
+        startTimerLine();
+
         ui.soruGoster(quiz.soruGetir());
         ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
-
-        console.log(quiz);
-
+        ui.btnNext.classList.remove("show");
     } else {
-        console.log("quiz bitti");
+        ui.scoreBox.classList.add("active");
+        ui.quizBox.classList.remove("active");
         ui.skoruGoster(quiz.dogruCevapSayisi, quiz.sorular.length);
     }
 });
 
 function optionSelected(e) {
+    clearInterval(counter);
+    clearInterval(counterLine);
+
     let selectedElement = e.target;
 
     if(selectedElement.nodeName == "SPAN") {
         selectedElement = selectedElement.parentElement;
     }
-
     const cevap = e.target.textContent[0];
     const soru = quiz.soruGetir();
 
@@ -42,6 +57,7 @@ function optionSelected(e) {
 
     quiz.soruIndex += 1;
     ui.disableAllOption();
+    ui.btnNext.classList.add("show");
 }
 
 ui.btnQuit.addEventListener("click", function() {
@@ -51,7 +67,44 @@ ui.btnQuit.addEventListener("click", function() {
 ui.btnReplay.addEventListener("click", function() {
     quiz.soruIndex = 0;
     quiz.dogruCevapSayisi = 0;
-    // star button
-    ui.btnNext.click();
-    ui.skoruGoster(quiz.dogruCevapSayisi, quiz.sorular.length);
+    // start button
+    ui.btnStart.click();
+    ui.scoreBox.classList.remove("active");
 });
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.timeSecond.textContent = time;
+        time--;
+
+        if(time < 0) {
+            clearInterval(counter);
+            ui.timeText.textContent = "Süre Bitti";
+
+            ui.disableAllOption();
+            quiz.soruIndex += 1;
+
+            ui.btnNext.classList.add("show");
+        }
+    }
+}
+
+let counterLine;
+function startTimerLine() {
+    let line_width = 0;
+
+    counterLine = setInterval(timer, 20);
+
+    function timer() {
+        line_width += 1;
+
+        ui.timeLine.style.width = line_width + "px";
+
+        if(line_width > 549) {
+            clearInterval(counterLine);
+        }
+    }
+}
